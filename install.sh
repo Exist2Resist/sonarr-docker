d376e3485e88eed48eb830d06bc31a09a6336940
Exist2Resist <exist2resist@outlook.com>
2022-01-04 14:14:17 -0700

    move mono install to own line
    mono was not installing if it was grouped in with the 4 line install and cleanup. moved the installation to a new line.

--- install.sh
+++ install.sh
@@ -5,19 +5,22 @@ su -c 'curl https://download.mono-project.com/repo/centos7-stable.repo | tee /et
 yum install epel-release yum-utils -y && yum clean all -y
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
-yum install wget mediainfo libzen libmediainfo curl gettext mono-core mono-devel sqlite.x86_64 -y && yum clean all -y
 
 yum -y install wget git par2cmdline p7zip unrar unzip tar gcc python-feedparser python-configobj \
-  && python-cheetah python-dbus python-devel libxslt-devel yum-utils \
+  && python-cheetah python-dbus python-devel libxslt-devel yum-utils mediainfo libzen libmediainfo \
+  && curl gettext sqlite.x86_64 \
   && yum clean all -y
 
-groupmod -g 100 users && usermod -u 99 -g 100 nobody 
+yum -y install mono-devel mono-core && yum clean all -y
+
+groupmod -g 100 users && usermod -u 99 -g 100 nobody
 
 mkdir -p /config
 
-wget http://download.sonarr.tv/v2/master/mono/NzbDrone.master.tar.gz -O /tmp/NzbDrone.master.tar.gz \
-  && tar -xvf /tmp/NzbDrone.master.tar.gz -C /opt/ \
-  && rm -f NzbDrone.master.tar.gz && mkdir /opt/sonarr && mkdir /opt/sonarr/bin && mv /opt/NzbDrone/* /opt/sonarr/bin \
-  && rm -rf /opt/NzbDrone && chown -R nobody:users /opt/sonarr /config
+# Packages list available here: http://services.sonarr.tv/v1/releases
+wget http://download.sonarr.tv/v3/main/3.0.6.1342/Sonarr.main.3.0.6.1342.linux.tar.gz -O /tmp/sonarr.tar.gz \
+  && tar -xvf /tmp/sonarr.tar.gz -C /opt/ \
+  && rm -f /tmp/sonarr.tar.gz && mkdir -p /opt/sonarr/bin && mv /opt/Sonarr/* /opt/sonarr/bin \
+  && rm -rf /opt/Sonarr && chown -R nobody:users /opt/sonarr /config
 
 systemctl enable sonarr.service
