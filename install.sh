3a02d5b462e64a0fdb9420cc5c2e7695d744863a
Exist2Resist <exist2resist@outlook.com>
2022-01-05 09:12:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 rpmkeys --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 su -c 'curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo'
 
-#yum install epel-release yum-utils -y && yum clean all -y
-#rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-#yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
-
 dnf -y update && dnf -y clean all
 
 dnf -y install wget git python36 par2cmdline p7zip unrar unzip tar gcc python-feedparser python-configobj \
@@ -13,15 +9,15 @@ dnf -y install wget git python36 par2cmdline p7zip unrar unzip tar gcc python-fe
   && curl gettext sqlite.x86_64 \
   && dnf clean all -y
 
-dnf -y install mono-devel-5.20.1.19 && yum clean all -y
-dnf -y install mono-core-5.20.1.19 && yum clean all -y
-# yum -y install mono-devel mono-core && yum clean all -y
+dnf -y install mono-devel-5.20.1.19 && dnf clean all -y
+dnf -y install mono-core-5.20.1.19 && dnf clean all -y
 
 useradd -u 99 sonarru && groupmod -g 100 users && usermod -u 99 -g 100 sonarru
 
 mkdir -p /config
 
 # Packages list available here: http://services.sonarr.tv/v1/releases
+# Need to add auto build
 wget http://download.sonarr.tv/v3/main/3.0.6.1342/Sonarr.main.3.0.6.1342.linux.tar.gz -O /tmp/sonarr.tar.gz \
   && tar -xvf /tmp/sonarr.tar.gz -C /opt/ \
   && rm -f /tmp/sonarr.tar.gz && mkdir -p /opt/sonarr/bin && mv /opt/Sonarr/* /opt/sonarr/bin \
